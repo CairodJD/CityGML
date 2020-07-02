6511121de5bf2bae207cd5d005e87dbabde65592
CairodJD <37753956+CairodJD@users.noreply.github.com>
2020-07-02 16:44:38 +0200

    Data structure and Helpers

---
+++ Assets/Scripts/DataStructure/Plane.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//https://www.habrador.com/tutorials/math/
+public class Plane : MonoBehaviour{
+    public Vector3 pos;
+
+    public Vector3 normal;
+
+    public Plane(Vector3 pos, Vector3 normal) {
+        this.pos = pos;
+
+        this.normal = normal;
+    }
+}
